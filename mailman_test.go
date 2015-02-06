7f64f8b95dfa2fe89a9b84d7aef9679e8074ea02
stealthycoin <john.w.carlyle@gmail.com>
2015-02-06 14:04:22 -0800

    Heavier test

--- mailman_test.go
+++ mailman_test.go
@@ -13,21 +13,20 @@ var (
 	TestResults chan string
 )
 
-func TestBulk(t *testing.T) {
+func BenchmarkBulk(b *testing.B) {
 	// Seed rng and create a channel to recieve test results on
 	rand.Seed(time.Now().UTC().UnixNano())
 	TestResults = make(chan string)
 
 	// Create a collector with three workers
 	InitCollector(3)
-	iterations := 1000 // Number of work requests to release
+	iterations := 10000 // Number of work requests to release
 
 	for i := 0 ; i < iterations ; i++ {
 		// Random delay from 1 to 10 seconds
 		delay := rand.Intn(10) + 1
 		IssueWorkRequest(NewWorkRequest(strconv.Itoa(i), "expected " + strconv.Itoa(delay) + "s", delay))
 	}
-
 
 	// Wait for the results and print them out for now
 	errors := 0
@@ -43,7 +42,7 @@ func TestBulk(t *testing.T) {
 		}
 	}
 	if errors > 0 {
-		t.Errorf("%d message(s) not delivered on time.\n", errors)
+		b.Errorf("%d message(s) not delivered on time.\n", errors)
 	} else {
 		fmt.Printf("All messages were delivered on time.\n")
 	}
