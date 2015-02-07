6f42cbfa4b7a37641dcef3953e1625c23e23a2fd
stealthycoin <john.w.carlyle@gmail.com>
2015-02-06 17:30:26 -0800

    [master fc46bd7] Cancel old requests and measure by delivery time not sleep time  3 files changed, 41 insertions(+), 26 deletions(-)  rewrite endpoints.go (64%)

--- work_request.go
+++ work_request.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"time"
+_	"fmt"
 )
 
 type WorkRequest struct {
 	uid string
 	payload string
-	timeout int
-	start_time int64
+	timestamp int64
 	cancel chan bool
 	valid bool
 }
 
 
 // Construct a new work request
-func NewWorkRequest(uid, payload string, timeout int) *WorkRequest {
+func NewWorkRequest(uid, payload string, timestamp int64) *WorkRequest {
 	return &WorkRequest{
 		uid: uid,
 		payload: payload,
-		timeout: timeout,
-		start_time: time.Now().UnixNano(),
+		timestamp: timestamp,
 		cancel: make(chan bool),
 		valid: true,
 
@@ -31,20 +30,29 @@ func NewWorkRequest(uid, payload string, timeout int) *WorkRequest {
 // Waiting process
 func (wr *WorkRequest) StartTimer() {
 	timeout := make(chan bool)
+
+	// Start listening routine
 	go func() {
-		time.Sleep(time.Duration(wr.timeout) * time.Second)
-		timeout <- true
-	}()
-	running := true
-	for running {
-		select {
-		case <- timeout:
-			if wr. valid {
-				workQueue <- wr
+		running := true
+		for running {
+			select {
+			case <- timeout:
+				if wr. valid {
+					workQueue <- wr
+				}
+				running = false
+			case <- wr.cancel:
+				wr.valid = false
 			}
-			running = false
-		case <- wr.cancel:
-			wr.valid = false
 		}
+	}()
+
+	// Trigger the timeout in the listening routine after sleeping
+	sleepTime := wr.timestamp - time.Now().UnixNano()
+	if sleepTime <= 0 {
+		timeout <- true
+	} else {
+		time.Sleep(time.Duration(sleepTime) * time.Nanosecond)
+		timeout <- true
 	}
 }
