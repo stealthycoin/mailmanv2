87d544c160e945fb32df6cff4d9f06deab7effdd
stealthycoin <john.w.carlyle@gmail.com>
2015-02-10 15:29:35 -0800

    Five seconds update

--- fiveseconds.go
+++ fiveseconds.go
@@ -1,12 +1,17 @@
 package main
 
-import "time"
+import (
+	"time"
+	"fmt"
+)
 
 func main() {
 	InitCollector(1)
 	InitPersist()
 	fiveSeconds := time.Now().UnixNano() + 5000000000
+	fmt.Println(requests)
 	IssueWorkRequest(NewWorkRequest("ID", "println", "This is five seconds later", fiveSeconds))
+	fmt.Println(requests)
 	time.Sleep(time.Duration(6) * time.Second)
 	StopCollector()
 }
