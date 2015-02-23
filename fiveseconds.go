3b630ec9b7ea88d873dd7b1e92407ffbc51b4401
stealthycoin <john.w.carlyle@gmail.com>
2015-02-23 13:14:20 -0800

    Fixed fivesecond test

--- fiveseconds.go
+++ fiveseconds.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"time"
-	"fmt"
 	"strconv"
 )
 
 func main() {
+	InitConfig()
 	InitCollector(1)
 	InitPersist()
 	fiveSeconds := time.Now().UnixNano() + 5000000000
-	fmt.Println(requests)
-	collectRequest <- NewWorkRequest(strconv.FormatInt(time.Now().UnixNano(), 10), "println", "This is five seconds later", fiveSeconds)
-	collectRequest <- NewWorkRequest("ID", "println", "This is five seconds later", fiveSeconds)
-	fmt.Println(requests)
+	collectRequest <- NewWorkRequest(strconv.FormatInt(time.Now().UnixNano(), 10), "println", "3", "This is five seconds later", fiveSeconds)
 	time.Sleep(time.Duration(6) * time.Second)
 	StopCollector()
 }
