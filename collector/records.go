efaf4a21c6ef09ae874dbd83757a0855c7256810
stealthycoin <john.w.carlyle@gmail.com>
2015-05-06 15:59:28 -0700

    Try with custom stuff

--- collector/records.go
+++ collector/records.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"log"
 	"time"
 	"container/heap"
 )
@@ -69,12 +70,13 @@ func CheckRecord(id string) bool {
 //
 // Go routine for recording
 //
-func StartRecords() {
+func StartRecords(duration_fmt string) {
+	log.SetFlags(log.Lshortfile)
+
 	// Heap for timing
 	h := &MailHeap{}
 	heap.Init(h)
 	counting := false
-	var timer time.Timer
 
 	// Hashmap for quick-check
 	active_records := make(map[string]int)
@@ -102,19 +104,32 @@ func StartRecords() {
 
 					go func() {
 						// Wait 5 minutes and then clean
-						<- time.After(5 * time.Minute)
-						clean <- true
+						if dur, err := time.ParseDuration(duration_fmt); err == nil {
+							<- time.After(dur)
+							clean <- true
+						} else {
+							log.Fatal(err)
+						}
 					}()
 				}
-
+			case rq := <- check_record:
+				// Check if a record exists
+				if count, ok := active_records[rq.id]; ok && count > 0{
+					rq.result <- true
+				}
+				rq.result <- false
 
 			case <- clean:
+				// Clean record storage
+				log.Println("Cleaning")
 				// Timer went off time to clean records
+				counting = false
 				now := time.Now().Unix()
 				done := false
 
 				// Keep deleting entries until we find one that is too young
 				for !done && h.Len() > 0 {
+					// Get oldest record
 					mr := (*h)[0]
 
 					// Check if it is from more than 5 minutes ago
@@ -124,12 +139,22 @@ func StartRecords() {
 						if active_records[mr.Uid] <= 0 {
 							delete(active_records, mr.Uid)
 						}
-					}
+					} else {
+						// This element is too young to delete but it is the oldest one
+						// Reset timer to when this needs to be deleted
+						done = true
+						counting = true
+						go func() {
+							delay, err := time.ParseDuration(duration_fmt)
+							if err != nil {
+								log.Fatal(err)
+							}
+							delay_secs := delay.Seconds()  - float64(now - mr.Last_alert)
 
-					// This element is too young to delete but it is the oldest one
-					// Reset timer to when this needs to be deleted
-					delay := (5 * 60) - (now - mr.Last_alert)
-					timer.Reset(time.Duration(delay) * time.Second)
+							<- time.After(time.Duration(delay_secs) * time.Second)
+							clean <- true
+						}()
+					}
 				}
 			}
 		}
