ea22e8e48e17d47dcbe22a9d41ddc837976604e2
stealthycoin <john.w.carlyle@gmail.com>
2015-05-08 15:13:39 -0700

    5m record init

--- server.go
+++ server.go
@@ -38,6 +38,7 @@ func main() {
 	wc, _ := strconv.Atoi(collector.Config["workers"])
 	collector.InitCollector(wc)
 	collector.InitPersist()
+	collector.StartRecord("5m")
 
 	// Handler function for requests
 	http.HandleFunc("/push/", collector.RequestHandler)
